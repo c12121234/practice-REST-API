ba15c4c0844c1d192f735bdc7a74893c95ea95f7
c12121234 <ko27tye@hotmail.com>
2020-12-30 20:16:29 +0800

    added HTTP get,post,put,patch,delete method in APIView
    Signed-off-by: c12121234 <ko27tye@hotmail.com>

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloAPIView(APIView):
+    serializer_class = serializers.HelloSerializer
     
     def get(self,request,format=None):
         """return a list of APIView features"""
@@ -12,3 +16,28 @@ class HelloAPIView(APIView):
             'Is mapped manually to URLs.',
         ]
         return Response({'message':'hello!','an_apiview':an_apiview})
+    
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    
+    def put(self,request,pk=None):
+        """updating an object"""
+        return Response({'method':'PUT'})
+    
+    def patch(self,request,pk=None):
+        """ a partial update of object"""
+        return Response({'method':'PATCH'})
+    
+    def delete(self,request,pk=None):
+        """ delete an object"""
+        return Response({'method':'DELETE'})
